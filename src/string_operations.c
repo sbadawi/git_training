c542b21c2c6363d92787605520ee848ab4e4bba5
sbadawi <sbadawi@nvidia.com>
2022-01-05 12:17:54 +0200

    Add strchr function for wide character string

--- src/string_operations.c
+++ src/string_operations.c
@@ -1,6 +1,16 @@
 #include <stdio.c>
 #include <wchar.h>
 
+wchar_t *my_wstrchr(wchar_t *ws, wchar_t wc){
+
+   while (*ws){
+    if (*ws == wc)
+      return ws;
+    ++ws;
+   }
+    return NULL;
+}
+
 size_t my_wstrlen(const wchar_t *s)
 {
     const wchar_t *p = s;
